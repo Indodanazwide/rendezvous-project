8269091fb26f8ed56a044e430296dea4ee42e44b
Indodanazwide <nxumalobukeka019@gmail.com>
2024-11-26 22:33:27 -0800

    Resetting the project

--- client/src/pages/Menu.jsx
+++
@@ -1,9 +0,0 @@
-const Menu = () => {
-    return (
-        <div className="menu-page">
-            <h1>Menu page</h1>
-        </div>
-    )
-}
-
-export default Menu
--- client/src/pages/Reservation.jsx
+++
@@ -1,9 +0,0 @@
-const Reservation = () => {
-    return (
-        <div className="reservation-page">
-            <h1>Reservation</h1>
-        </div>
-    )
-}
-
-export default Reservation
